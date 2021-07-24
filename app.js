bfeba6815884fc064b2fe02169bf02ceef9e6392
Alp Yalcinkaya <yalcinkayaalp@gmail.com>
2021-07-24 14:14:49 +0300

    add data presets to be used in traffic breakdown

--- app.js
+++ app.js
@@ -17,9 +17,44 @@ let dataPreset1 = {
 
 // Data Preset 2
 
+let dataPreset2 = {
+    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        datasets: [{
+            data: [2500, 5000, 8000, 9000, 8500, 6000, 2000, 9000, 7500, 7000, 9500],
+            backgroundColor: 'rgba(211,212,234,.3)',
+            tension: 0.4,
+            borderWidth: 1,
+            fill: true
+        }]
+
+};
+
 // Data Preset 3
 
+let dataPreset3 = {
+    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        datasets: [{
+            data: [2000, 2500, 1000, 4000, 6000, 3000, 5000, 9000, 8000, 10000, 9500],
+            backgroundColor: 'rgba(211,212,234,.3)',
+            tension: 0.4,
+            borderWidth: 1,
+            fill: true
+        }]
+
+};
+
 // Data Preset 4
+
+let dataPreset4 = {
+    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        datasets: [{
+            data: [1000, 2500, 1000, 3000, 5000, 2000, 5000, 9000, 8000, 10000, 9500],
+            backgroundColor: 'rgba(211,212,234,.3)',
+            tension: 0.4,
+            borderWidth: 1,
+            fill: true
+        }]
+};
 
 // Chart Options
 
@@ -27,7 +62,7 @@ let chartOptions = {
     aspectRatio: 2.5,
     responsive: true,
     animation: {
-        duration: 1000 * 1,
+        duration: 600,
         easing: 'linear'
     },
     scales: {
